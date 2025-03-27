23d50600888266e42fd274df5f6bd75a626d88c1
Sree S. <sanakkayala12@gmail.com>
2025-03-27 17:27:05 +0000

    fix the tranche.sol

--- contracts/Tranche.sol
+++ contracts/Tranche.sol
@@ -1,21 +1,24 @@
 // SPDX-License-Identifier: MIT
 
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "ITranche.sol";
 
-contract Tranche is ERC20, Ownable {
+contract Tranche is ERC20, Ownable, ITranche {
     constructor(
         string memory name_,
         string memory symbol_
     ) ERC20(name_, symbol_) {}
 
-    function mint(address account, uint256 amount) public onlyOwner {
+    function mint(address account, uint256 amount) external onlyOwner {
         _mint(account, amount);
     }
 
-    function burn(address account, uint256 amount) public onlyOwner {
+    function burn(address account, uint256 amount) external onlyOwner {
         _burn(account, amount);
     }
 }
